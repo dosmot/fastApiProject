7ea002fe4ed2fc353d58e462148da73e0776ff3c
erkyl <kurokyxoxo@yandex.ru>
2025-01-24 15:00:40 +0500

    Добавлены таблицы для дб profiles, desc_profiles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from passlib.context import CryptContext
 from authx import AuthXConfig, AuthX, RequestToken, TokenPayload
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel, Field
@@ -14,7 +15,13 @@ config = AuthXConfig(
 )
 auth = AuthX(config)
 auth.handle_errors(app)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 #############################база данных###################################
 engine = create_async_engine('postgresql+asyncpg://postgres:123@localhost/Project_test') #тип бд+движок://логин:пасс@host/db
 new_session = async_sessionmaker(engine) # создание асинхсессии
@@ -29,11 +36,29 @@ class accounts_db(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     login: Mapped[str]
     password: Mapped[str]
+
+class profiles_db(Base):
+    __tablename__ = 'profiles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    account_id: Mapped[int] = mapped_column(foreign_key=accounts_db.id)
+
+class desc_profiles_db(Base):
+    __tablename__ = 'desc_profiles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    birth_date: Mapped[str]
+    avatar: Mapped[str]
+    profile_id: Mapped[int] = mapped_column(foreign_key=profiles_db.id)
 #############################модели валидации###################################
 
 class Users_Model(BaseModel):
     login: str = Field(min_length=6, max_length=30)
     password: str = Field(min_length=6, max_length=30)
+#class Profiles_Model(BaseModel):
+
 @app.post('/create_db',tags=["База данных"])     #создание таблицы
 async def create_db():
     async with engine.begin() as conn:
@@ -46,8 +71,8 @@ async def add_account(data: Users_Model, session: AsyncSession = Depends(get_db)
         exist_user = res.scalars().first()
         if exist_user:
             raise HTTPException(400, detail="Пользователь уже существует")
-
-        new_account = accounts_db(login=data.login, password=data.password)
+        hash_pass = get_password_hash(data.password)
+        new_account = accounts_db(login=data.login, password=hash_pass)
         session.add(new_account)
         await session.commit()
         return {"msg": True}
@@ -57,23 +82,31 @@ async def add_account(data: Users_Model, session: AsyncSession = Depends(get_db)
 
 @app.post("/auth")
 async def login(data: Users_Model,session : AsyncSession = Depends(get_db)): #роут для авторизации
-    req = select(accounts_db).where(accounts_db.login == data.login and accounts_db.password == data.password )
+   # req = select(accounts_db).where(accounts_db.login == data.login and accounts_db.password == data.password )
+    req = select(accounts_db).where(accounts_db.login == data.login)
     result = await session.execute(req)
     if result is not None:
         row = result.scalars().first()
-        access_token = auth.create_access_token(uid=str(row.id),fresh=True)
-        refresh_token = auth.create_refresh_token(uid=str(row.id))
-        return {"id":row.id,"login":row.login,"access_token": access_token,"refresh_token":refresh_token}
+        pass_bool = verify_password(data.password, str(row.password))
+        if pass_bool:
+            access_token = auth.create_access_token(uid=str(row.id),fresh=True)
+            refresh_token = auth.create_refresh_token(uid=str(row.id))
+            return {"id":row.id,"login":row.login,"access_token": access_token,"refresh_token":refresh_token}
+        else:
+            return {"msg":"Неправильный логин или пароль"}
     else:
         raise HTTPException(401, detail="invalid credentials")
    # await session.commit()
-
+@app.post("/logout") #ДОБАВИТЬ ОТЗЫВ РЕФРЕШ ТОКЕНА
+def logout(token: RequestToken = Depends(auth.access_token_required)):
+    token.revoke_token(token)
+    return {"ok": True}
 @app.post("/refresh")
 def refresh(token: TokenPayload = Depends(auth.refresh_token_required)):
     access_token = auth.create_access_token(token.sub)
     return {"access_token": access_token}
-
-@app.get("/profile_view", dependencies=[Depends(auth.access_token_required)]) #протектед роут
+#########################Необходимо разбирать на роуты#####################################
+@app.get("/profile_view", dependencies=[Depends(auth.get_token_from_request)]) #протектед роут
 def profile_view(token: RequestToken = Depends()):
     try:
         auth.verify_token(token=token)
