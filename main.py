78b8a3c7738536ea0790e7f420711a4806b25bd9
erkyl <kurokyxoxo@yandex.ru>
2025-02-06 17:47:52 +0500

    добавлено сохранение профиля, надо проверить

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil
+import traceback
 from datetime import timedelta, date
 from typing import Annotated
 import uvicorn
@@ -17,11 +18,11 @@ config = AuthXConfig(
     JWT_ALGORITHM="HS256",
     JWT_SECRET_KEY="SecretKey",
     JWT_TOKEN_LOCATION=['json'],
-    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30),
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=15),
     JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=7)
 )
-auth = AuthX(config)
-auth.handle_errors(app)
+authZ = AuthX(config)
+authZ.handle_errors(app)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 UPLOAD_DIR = "uploads"
@@ -81,7 +82,7 @@ class Profiles_desc(BaseModel):
     first_name: str = Field(min_length=2, max_length=300)
     last_name: str = Field(min_length=2, max_length=300)
     birth_date: date
-    avatar: FilePath
+    # avatar: FilePath
 
 @app.post('/create_db',tags=["База данных"])     #создание таблицы
 async def create_db():
@@ -107,7 +108,7 @@ async def add_account(data: Users_Model, session: AsyncSession = Depends(get_db)
         await session.rollback()
         raise HTTPException(400, detail="Пользователь уже существует")
 
-@app.post("/auth")
+@app.post("/authorization")
 async def login(data: Users_Model,session : AsyncSession = Depends(get_db)): #роут для авторизации
    # req = select(accounts_db).where(accounts_db.login == data.login and accounts_db.password == data.password )
     req = select(accounts_db).where(accounts_db.login == data.login)
@@ -116,50 +117,48 @@ async def login(data: Users_Model,session : AsyncSession = Depends(get_db)): #р
     if row is not None:
         pass_bool = verify_password(data.password, str(row.password))
         if pass_bool:
-            access_token = auth.create_access_token(uid=str(row.id),fresh=True)
-            refresh_token = auth.create_refresh_token(uid=str(row.id))
+            access_token = authZ.create_access_token(uid=str(row.id),fresh=True)
+            refresh_token = authZ.create_refresh_token(uid=str(row.id))
             return {"id":row.id,"login":row.login,"access_token": access_token,"refresh_token":refresh_token}
         else:
             return {"msg":"Неправильный логин или пароль"}
     else:
         raise HTTPException(401, detail="invalid credentials")
    # await session.commit()
-@app.post("/account/del_account")
-async def delete_account(
+@app.delete("/account/del_account")
+async def delete_account( tk : TokenPayload  = Depends(authZ.access_token_required),
                          session: AsyncSession = Depends(get_db)):
     try:
-        # vt = auth.verify_token(token)
-        # session.
-        # session.delete(res)
-        # await session.commit()
-        # print(res)
-        return res
+        res = await session.execute(select(accounts_db).where(accounts_db.id == int(tk.sub)))
+        find_acc = res.scalars().first()
+        await session.delete(find_acc)
+        await session.commit()
+        return {"msg":True}
     except Exception as e:
-        print(str(e))
-        return {"msg":False}
+        print(str(e), traceback.format_exc())
+        return {"msg":str(e)}
 
 
 @app.post("/logout") #ДОБАВИТЬ ОТЗЫВ РЕФРЕШ ТОКЕНА
-def logout(token: RequestToken = Depends(auth.access_token_required)):
+def logout(token: RequestToken = Depends(authZ.access_token_required)):
     token.revoke_token(token)
     return {"ok": True}
 @app.post("/refresh")
-def refresh(token: TokenPayload = Depends(auth.refresh_token_required)):
-    access_token = auth.create_access_token(token.sub)
+def refresh(token: TokenPayload = Depends(authZ.refresh_token_required)):
+    access_token = authZ.create_access_token(token.sub)
     return {"access_token": access_token}
 #########################Необходимо разбирать на роуты#####################################
 
 
 
 #########################       ПРОФИЛИ    #####################################
-@app.post("/account/create_profile", dependencies=[Depends(auth.get_token_from_request)])
-async def create_profile(data: Profiles_Model, session: AsyncSession = Depends(get_db), token: RequestToken = Depends()):
+@app.post("/account/create_profile")
+async def create_profile(data: Profiles_Model, session: AsyncSession = Depends(get_db), tk : TokenPayload  = Depends(authZ.access_token_required)):
     try:
-        vt = auth.verify_token(token=token)
         res = await session.execute(select(profiles_db).filter(profiles_db.username == data.username))
         if res.scalars().first() is None:
             hash_pass = get_password_hash(data.password)
-            session.add(profiles_db(username=data.username,password=hash_pass,account_id=vt.id))
+            session.add(profiles_db(username=data.username,password=hash_pass,account_id=int(tk.sub)))
             await session.commit()
             return {"create_profile": True}
         else:
@@ -168,22 +167,22 @@ async def create_profile(data: Profiles_Model, session: AsyncSession = Depends(g
         return {"create_profile":False,"error":str(e)}
 
 
-@app.get("/account/get_profiles", dependencies=[Depends(auth.get_token_from_request)])
-async def get_profiles( session: AsyncSession = Depends(get_db), token: RequestToken = Depends()):
+@app.get("/account/get_profiles")
+async def get_profiles( session: AsyncSession = Depends(get_db), tk : TokenPayload  = Depends(authZ.access_token_required)):
     try:
-        vt = auth.verify_token(token=token)
-        res = await session.execute(select(profiles_db.id).filter(profiles_db.account_id == vt.id))
-        if res.scalars().first() is None:
-            return {"Count_profiles": False}
-        else:
-            return res.scalars()
+        res = await session.execute(select(profiles_db).filter(profiles_db.account_id == int(tk.sub)))
+        prof = res.mappings().all()
+        # if res.scalars().first() is None:
+        #     return {"Count_profiles": False}
+        # else:
+        return prof
     except Exception as e:
         return {"Count_profiles":False,"error":str(e)}
 
-@app.get("/account/profile/{profile_id}/desc_profile", dependencies=[Depends(auth.get_token_from_request)])
+@app.get("/account/profile/{profile_id}/desc_profile", dependencies=[Depends(authZ.get_token_from_request)])
 async def get_profile(profile_id: str, session: AsyncSession = Depends(get_db), token: RequestToken = Depends()):
     try:
-        vt = auth.verify_token(token=token)
+        vt = authZ.verify_token(token=token)
         res = await session.execute(select(desc_profiles_db).filter(desc_profiles_db.profile_id == int(profile_id)))
         if res.scalars().first() is None:
             return {"desc_profiles": False}
@@ -193,23 +192,23 @@ async def get_profile(profile_id: str, session: AsyncSession = Depends(get_db), 
         return {"error":str(e)}
 
 
-@app.post("/account/profile/desc_profile_save", dependencies=[Depends(auth.get_token_from_request)], tags=["Работа с профилями"])
-async def save_profile(data: Profiles_desc, session: AsyncSession = Depends(get_db), token: RequestToken = Depends(), file: UploadFile = File(...)):
+@app.post("/account/profile/desc_profile_save", tags=["Работа с профилями"])
+async def save_profile(data: Profiles_desc, session: AsyncSession = Depends(get_db),
+                       file: UploadFile = File(...)):
     try:
-        vt = auth.verify_token(token=token)
         new_name = generate(file.filename, 21)
         file_path = os.path.join(UPLOAD_DIR, new_name)
         with open(file_path,"wb") as buff:
             shutil.copyfile(file.filename, file_path )
-        session.add(desc_profiles_db(first_name = data.first_name,last_name=data.last_name,birth_date=data.birth_date,avatar=file_path))
-        await session.commit()
+        # session.add(desc_profiles_db(first_name = data.first_name,last_name=data.last_name,birth_date=data.birth_date,avatar=file_path))
+        # await session.commit()
     except Exception as e:
         return {"error":str(e)}
 
-@app.get("/profile_view", dependencies=[Depends(auth.get_token_from_request)]) #протектед роут
+@app.get("/profile_view", dependencies=[Depends(authZ.get_token_from_request)]) #протектед роут
 def profile_view(token: RequestToken = Depends()):
     try:
-        auth.verify_token(token=token)
+        authZ.verify_token(token=token)
         return {"msg_access":True}
     except:
         raise HTTPException(401, detail="invalid credentials")
